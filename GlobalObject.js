beafb14a2f0aaf6787e1450a3711c6dd89494e5a
Arindom Kundu <arindomkundu18@gmail.com>
2021-06-10 22:17:03 +0600

    Common Global Objects

--- GlobalObject.js
+++ GlobalObject.js
@@ -1,1 +1,16 @@
-console.log("Hey Coders!!");
+//These objects are available in all modules. The following variables may appear to be global but are not. They exist only in the scope of modules,
+// console.log("Hey Coders!!");
+console.log(__dirname);
+console.log(__filename);
+var time = 0;
+var timer = setInterval(function(){
+  time += 2;
+  console.log(time+" second has passed");
+  if(time > 6)
+    clearInterval(timer);
+},3000);
+
+//it will print after 2 second pass though this function is written in lower order of the previosu funcntion
+setTimeout(function(){
+  console.log("printing after 2 seconds");
+},2000);
