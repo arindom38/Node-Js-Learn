5b5b3de2595b7cd47d346f51e1af48cac10bf5f5
Arindom Kundu <arindomkundu18@gmail.com>
2021-06-11 12:32:42 +0600

    Pass function into function using functinal expression

--- functionalExpression.js
+++ functionalExpression.js
@@ -11,3 +11,45 @@ var saybye = function(){
 };
 
 saybye();
+
+//calling a function as parameter from a function using funcational expression
+function mycommonfun(fun){
+  fun();
+};
+
+mycommonfun(sayhi);
+mycommonfun(saybye);
+
+//another example
+function areaprint(fun){
+  console.log(fun());
+};
+var sqr = function(){
+  return 3.1416*4*4;
+};
+var rectangle = function(){
+  return 10*20;
+};
+
+//calling functions
+areaprint(sqr);
+areaprint(rectangle);
+
+
+/*
+Syntax for Function Declaration:
+
+function functionName(x, y) { statements... return (z) };
+Syntax for Function Expression (anonymous) :
+
+let variableName = function(x, y) { statements... return (z) };
+Syntax for Function Expression (named) :
+
+let variableName = function functionName(x, y)
+{ statements... return (z) };
+Syantax for Arrow Function:
+
+
+
+let variableName = (x, y) => { statements... return (z) };
+*/
