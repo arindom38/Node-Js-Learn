4af420527559bcf8a8a4451e99d8398e500a702c
Arindom Kundu <arindomkundu18@gmail.com>
2021-06-12 21:13:24 +0600

    More about Modules

--- UserModule.js
+++ UserModule.js
@@ -12,9 +12,19 @@ const usersFirstName = function(arr){
     console.log("First name Of Usrer #"+i+" : "+arr[i]);
 };
 
+//String with value concation can be formatted in another way
+const userSalary = function(user,basic){
+    return `${user} get monthly: ${basic*20000}`;
+};
+
+//Not only function any static value van be passed here
+const company_name = 'Google';
 //Function need to be exported or other file can't access it
 module.exports = {
-  users,
-  usersCnt,
-  usersFirstName
+//Properties: object
+  users:users,
+  usersCnt: usersCnt,
+  usersFirstName:usersFirstName,
+  userSalary: userSalary,
+  company: company_name //app can access the properties through properties name
 };
