74d810eae5f78cb2be70e64e44c903eb2e5875b0
Arindom Kundu <arindomkundu18@gmail.com>
2021-06-12 22:04:10 +0600

    Event emiitter using util module

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //core module for events(build in)
 var events = require('events');
-
+//event emitter using util module
+var util = require('util');
+/* Simple Event example
 //customized event emitter constructor
 var myEmitter = new events.EventEmitter();
 
@@ -9,4 +11,36 @@ myEmitter.on('someEvent',function(mssg){
 });
 
 //Manualy emitting the event for test purpose
-myEmitter.emit('someEvent','The event is emitted!');
+myEmitter.emit('someEvent','The event is emitted!'); */
+
+//creating object with constructor
+var Person = function(name){
+  this.name = name;
+};
+
+//inheriting util for Person object
+util.inherits(Person,events.EventEmitter);
+
+//creating some Person object instances
+var aro = new Person('aro');
+var mahidul = new Person('mahidul');
+var pijush = new Person('pijush');
+//array of person instances
+var people = [aro,mahidul,pijush];
+
+//custom evenet for each person in the people array
+people.forEach((Person) => {
+  Person.on('speak',function(mssg){
+    console.log(Person.name + ' said: '+ mssg);
+  });
+  Person.on('work',mssg =>{
+    console.log(Person.name+' work: '+ mssg);
+  });
+});
+
+
+//event calling
+aro.emit('speak','Hey brossss!');
+mahidul.emit('speak',"Let's Eat togehther!");
+pijush.emit('speak','I have works to do, later !!');
+pijush.emit('work','working for 2 hrs');
