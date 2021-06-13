d59da5266cac8d6225e60acbf18aa5934f2bdd36
ujjal.kundu <arindomkundu18@gmail.com>
2021-06-13 16:05:01 +0600

    Directory creating and removing

--- appfile.js
+++ appfile.js
@@ -1,29 +1,17 @@
 //core module for file reading writing
 var fs = require('fs');
-/*
-//synchronous method:  will block the rest of the code after this line untill the file reading /writeing is finished
-var readMe = fs.readFileSync('readMe.txt','utf8');
-var writeMe = fs.writeFileSync('writeMe.txt',readMe);*/
 
-//asynchornous method: will not block any code while i/0 operation
-//asynchornous read/write method take callback funtion for capturing error or data as third paramter, it mandatory
-//callback function is called once the i/o operation in completed
-fs.readFile('readMe.txt','utf8',(err,data) => {
-  fs.writeFile('writeMe.txt',data,(err) => {
-    if(err == null)
-      console.log('File written complete!');
-  });
+//mkdir() asynchornous method: this will create a directory
+fs.mkdir('subdir',(err) =>{
+  console.log('directory is created!');
 });
-console.log('I am not blocked and I came first!');
 
-/*
---for deleting files--
-fs.unlink('writeMe.txt',(err) =>{
-  if(err) throw err;
-  console.log('File is deleted! ');
+
+// fs.mkdir('subdir1',(err) =>{
+//   console.log('directory is created!');
+// });
+
+//removing directory
+fs.rmdir('subdir1',(err) => {
+    console.log('directory is deleted!');
 });
-*/
-/*output
-I am not blocked and I came first!
-File written complete!
-*/
