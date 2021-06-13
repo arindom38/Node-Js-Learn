94d09942bdd87e93eb8ab21e45fd75bb549863c4
ujjal.kundu <arindomkundu18@gmail.com>
2021-06-13 16:23:04 +0600

    Directory handleing example is added

--- appfile.js
+++ appfile.js
@@ -2,16 +2,27 @@
 var fs = require('fs');
 
 //mkdir() asynchornous method: this will create a directory
-fs.mkdir('subdir',(err) =>{
-  console.log('directory is created!');
-});
-
-
-// fs.mkdir('subdir1',(err) =>{
+// fs.mkdir('subdir',(err) =>{
 //   console.log('directory is created!');
 // });
 
-//removing directory
-fs.rmdir('subdir1',(err) => {
-    console.log('directory is deleted!');
+/*
+--First run this--
+fs.mkdir('subdir',(err) =>{
+  if(err) throw err;
+  fs.readFile('readMe.txt','utf8',(err,data)=>{
+    fs.writeFile('./subdir/writeMe.txt',data,(err)=>{
+      if(err) throw err;
+      console.log('File is written in sub directory');
+    });
+  });
+});
+*/
+// to remove a emepty directory
+//fs.rmdir('subdir',()=>{});
+
+//to remove a directory that contain files must remove the file fast then remove the directory
+//after creating directory using abode code the comment them and run the below code
+fs.unlink('./subdir/writeMe.txt',()=>{
+  fs.rmdir('subdir',()=>{});
 });
