939e29b47d84d9ba2cf213ae7cebe102927ac1ea
ujjal.kundu <arindomkundu18@gmail.com>
2021-06-14 16:34:57 +0600

    sending json data to server in response

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer((req,res)=>{
-  res.writeHead(200,{'Content-Type':'text/html'});
-  var myReadSteam = fs.createReadStream(__dirname+'/index.html','utf8');
-  myReadSteam.pipe(res);
+  res.writeHead(200,{'Content-Type':'application/json'}); //type must be json
+  var myObj = {
+    name: 'Arindom',
+    Job: 'Programmer',
+    Salary : '444000.00',
+    country: 'Bangladesh'
+  };
+  res.end(JSON.stringify(myObj)); //sending obj as a json
 });
 
 server.listen(3000,'127.0.0.1');
