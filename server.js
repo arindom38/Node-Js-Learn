77329a1eacdafeb2472bfd341b8ef515ef7930df
ujjal.kundu <arindomkundu18@gmail.com>
2021-06-14 12:54:24 +0600

    Fixation on realod data using pipe method

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 
 var http = require('http');
-var pipeStream = require('./pipeStream').myReadSteam;
+var fs = require('fs');
+//var pipeStream = require('./pipeStream').myReadSteam;
 
 var server = http.createServer((req,res)=>{
   console.log('request is made: '+req.url);
   res.writeHead(200,{'Content-Type':'text/plain'});
-  pipeStream.pipe(res);
+  //this callback function will execute everytime a request is send
+  // if we use it by exporting from another module (pipeStream.js) with reload it can't read file
+  var myReadSteam = fs.createReadStream(__dirname+'/readMe.txt','utf8');
+  myReadSteam.pipe(res);
 });
 
 server.listen(3000,'127.0.0.1');
