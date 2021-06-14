0fc6a5f1da99900a81def40eab967479b1ed8618
ujjal.kundu <arindomkundu18@gmail.com>
2021-06-14 10:45:01 +0600

    Testing server for plain text content type

--- appserver.js
+++ appserver.js
@@ -2,8 +2,8 @@ var http = require('http');
 
 var server = http.createServer((req,res)=>{
   console.log('request is made: '+req.url);
-  res.writeHead(200,{'Content-Type':'text/html'});
-  res.end('<h2>Hello Galaxy!</h2>');
+  res.writeHead(200,{'Content-Type':'text/text'});
+  res.end('Hello Galaxy!');
 });
 
 server.listen(3000,'127.0.0.1');
