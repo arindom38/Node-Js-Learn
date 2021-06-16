92667cc61450226514bc80c513fba499feab1873
Arindom Kundu <arindomkundu18@gmail.com>
2021-06-17 01:36:02 +0600

    handling form post request using body-parser module

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 //return a function
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 //for rendering dynamic data into view need ejs template engine
 //view engine will automatically serach *.ejs file in views folder
 app.set('view engine', 'ejs');
-//middle ware used for loading static files like css
+//middle ware used for loading static files like css/json etc
 //the first paramter in use function indicating  the href link in ejs/html file .eg href="/resources/style.css"
 //2nd paramtet indicating the actual path of the css file
 // details : https://expressjs.com/en/guide/using-middleware.html#middleware.router
 app.use('/resources',express.static('assets'));
 
+//middle-ware for form submit post request ,request is pass through request body
+var urlencodedParser = bodyParser.urlencoded({extended:false});
+
+//request handling
 app.get('/',(req,res)=>{
   res.render('index');
 });
 
 app.get('/contact',(req,res)=>{
   res.render('contact',{qs: req.query}); //sending the query string like ..../contact?dept=IT&person=joye
+});
+
+app.post('/contact',urlencodedParser,(req,res)=>{
+  console.log(req.body);
+  res.render('contact-success',{data: req.body});
 });
 
 //id is a varaible ,can be used for dynamic routing
