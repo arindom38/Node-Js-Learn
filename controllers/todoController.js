04b20f3d80c0a6e0db7d4418cf5eb6517f4d7d9e
ujjal.kundu <arindomkundu18@gmail.com>
2021-06-17 15:11:59 +0600

    data rendiering in view from controller

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,7 +1,9 @@
+//dummy data
+var data = [{item: 'Take Meal'},{item: 'Kick some Coding'},{item: 'Take a nap'},];
 module.exports = (app) => {
 
 app.get('/todo',(req,res)=>{
-  res.render('todo');
+  res.render('todo',{todos: data});
 });
 
 app.post('/todo',(req,res)=>{
