0e1adf101c46fa7e41245c673bc81520175caea5
ujjal.kundu <arindomkundu18@gmail.com>
2021-06-17 15:37:35 +0600

    post request handling and updating view dynamically

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,13 +1,16 @@
 //dummy data
-var data = [{item: 'Take Meal'},{item: 'Kick some Coding'},{item: 'Take a nap'},];
+var data = [{item: 'Take Meal'},{item: 'Kick some Coding'},{item: 'Take a nap'}];
+var urlencodeParser = require('express').urlencoded({extended:true});
 module.exports = (app) => {
 
 app.get('/todo',(req,res)=>{
   res.render('todo',{todos: data});
 });
 
-app.post('/todo',(req,res)=>{
-
+app.post('/todo',urlencodeParser,(req,res)=>{
+  //adding the new item in the existing data
+  data.push(req.body);
+  res.json(data); //can also be send as res.json({todos: data})
 });
 
 app.delete('/todo',(req,res)=>{
