f364dce8fcaf402fb57c2fd31a84124f6633c445
ujjal.kundu <arindomkundu18@gmail.com>
2021-06-17 12:29:23 +0600

    set up controllers

---
+++ app.js
@@ -0,0 +1,17 @@
+//imported module
+var express = require('express');
+var todoController = require('./controllers/todoController');
+//express fire function
+var app = express();
+
+//set up template engine
+app.set('viw engine','ejs');
+
+//middle-ware for assets management
+app.use('/assets',express.static('./public'));
+
+//Handle controllers
+todoController(app);
+//listen to port
+app.listen(3000);
+console.log('Your app is listenting to port 3000');
